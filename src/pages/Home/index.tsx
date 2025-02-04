658397ab21675a999bcb531c5e765be79c78bb7c
Vitor Vianna <vitor.vianna.10@gmail.com>
2025-02-04 17:24:37 -0300

    Estilização dos inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
   return (
@@ -7,10 +7,29 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput 
+          id="task" 
+          placeholder="Dê um nome ao seu projeto."
+          list="task-suggestion"
+          />
+
+          <datalist id="task-suggestion">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinutesAmountInput 
+          type="number" 
+          id="minutesAmount" 
+          placeholder="00"
+          step={5}
+          min={5}
+          max={60}
+
+          />
 
           <span>minutos.</span>
         </FormContainer>
@@ -23,10 +42,10 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
+        <StartCountdownButton disabled type="submit">
           <Play size={24}/>
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
