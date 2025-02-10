82258302c50056e020f3380229ae67bb41b8e87c
Vitor Vianna <vitor.vianna.10@gmail.com>
2025-02-10 18:23:18 -0300

    Correção de bugs na funcionalidade do timer e inclusão da exibição do timer no título da aba do navegador

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,10 +54,16 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-        setInterval(() => {
+        interval = setInterval(() => {
           setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
         }, 1000)
+      }
+
+      return () => {
+        clearInterval(interval)
       }
     }, [activeCycle])
 
@@ -72,6 +78,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     
     reset();
   }
@@ -84,6 +91,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `Ignite - Timer ${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
